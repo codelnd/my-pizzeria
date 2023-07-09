4838ab2ca7f5599afbdda86363cecf509e9368d3
Denis Bezukladnikov <denis3094@mail.ru>
2023-07-09 21:34:52 +0700

    feat: создать категории

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,13 @@ function App() {
         <div className='wrapper'>
             <ul>
                 <li className='active'>
-                    
+                    Все
                 </li>
+                <li>2</li>
+                <li>3</li>
+                <li>4</li>
+                <li>5</li>
+                <li>6</li>
             </ul>
         </div>
     )
