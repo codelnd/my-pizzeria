04c94ebbfd4c66959d9c3b620f9942afd5cd1924
Denis Bezukladnikov <denis3094@mail.ru>
2023-07-15 02:41:38 +0700

    feat: создать основные компоненты

---
+++ src/components/Categories/Categories.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Categories = () => {
+    return (
+        <section className="categories">
+            <ul>
+                <li className="active">Все</li>
+                <li>Мясные</li>
+                <li>Вегетарианская</li>
+                <li>Гриль</li>
+                <li>Острые</li>
+                <li>Закрытые</li>
+            </ul>
+        </section>
+    );
+};
+
+export default Categories;
