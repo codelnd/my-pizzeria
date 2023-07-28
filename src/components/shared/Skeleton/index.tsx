53ddf8bebceebf6bf488bf9eb0da36ab1a3e83ff
Denis Bezukladnikov <denis3094@mail.ru>
2023-07-28 23:28:09 +0700

    feat: создать общую компоненту скелетон

---
+++ src/components/shared/Skeleton/index.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import ContentLoader from "react-content-loader";
+
+const Skeleton = (props) => (
+  <ContentLoader
+    speed={2}
+    width={280}
+    height={464}
+    viewBox="0 0 280 464"
+    backgroundColor="#f3f3f3"
+    foregroundColor="#ecebeb"
+    {...props}
+  >
+    <circle cx="140" cy="129" r="130" />
+    <rect x="0" y="275" rx="10" ry="10" width="280" height="27" />
+    <rect x="1" y="315" rx="10" ry="10" width="280" height="87" />
+    <rect x="0" y="422" rx="10" ry="10" width="91" height="27" />
+    <rect x="140" y="412" rx="10" ry="10" width="137" height="44" />
+  </ContentLoader>
+);
+
+export default Skeleton;
