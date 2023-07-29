c87264ef0389425c52ae0af13fea9b94952e3e4c
Denis Bezukladnikov <denis3094@mail.ru>
2023-07-30 00:54:26 +0700

    feat: сделать навигацию через react-router

--- src/components/NotFound/index.tsx
+++ src/components/NotFound/index.tsx
@@ -1,11 +1,19 @@
 import React from "react";
 import s from "./NotFound.module.scss";
+import { useNavigate } from "react-router-dom";
 
 const NotFound = () => {
+  const navigate = useNavigate();
+
   return (
     <div className={s.container}>
       <h1>Такой страницы не существует</h1>
-      <button className="button button--outline button--add">Назад</button>
+      <button
+        onClick={() => navigate(-1)}
+        className="button button--outline button--add"
+      >
+        Назад
+      </button>
     </div>
   );
 };
