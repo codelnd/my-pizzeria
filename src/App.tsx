a58ba7663154eb845839f46c670973fa34c8de78
Denis Bezukladnikov <denis3094@mail.ru>
2023-07-29 23:25:41 +0700

    feat: вынести главную страницу в папку pages, установить модуль react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,18 @@
 import React from "react";
 import "./assets/scss/app.scss";
 import Header from "./components/Header/Header";
-import Categories from "./components/Categories/Categories";
-import Sort from "./components/Sort/Sort";
-import Pizza from "./components/Pizza/Pizza";
-import Skeleton from "./components/shared/Skeleton";
+import Home from "./pages/Home";
+import { Route, Routes } from "react-router";
 
 function App() {
-  const [items, setItems] = React.useState([]);
-  const [isLoading, setIsLoading] = React.useState(true);
-
-  React.useEffect(() => {
-    fetch(`https://64c0064d0d8e251fd111d86b.mockapi.io/items`)
-      .then((res) => res.json())
-      .then((data) => {
-        setItems(data);
-        setIsLoading(false);
-      });
-  }, []);
-
   return (
     <div className="wrapper">
       <Header />
       <main className="content">
         <div className="container">
-          <div className="content__top">
-            <Categories />
-            <Sort />
-          </div>
-          <h2 className="content__title">Все пиццы</h2>
-          <div className="content__items">
-            {isLoading
-              ? [...new Array(8)].map((_, i) => <Skeleton key={i} />)
-              : items.map((el) => <Pizza key={el.id} {...el} />)}
-          </div>
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
         </div>
       </main>
     </div>
---
+++ src/pages/Home.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import Categories from "../components/Categories/Categories";
+import Sort from "../components/Sort/Sort";
+import Pizza from "../components/Pizza/Pizza";
+import Skeleton from "../components/shared/Skeleton";
+
+const Home = () => {
+  const [items, setItems] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(true);
+
+  React.useEffect(() => {
+    fetch(`https://64c0064d0d8e251fd111d86b.mockapi.io/items`)
+      .then((res) => res.json())
+      .then((data) => {
+        setItems(data);
+        setIsLoading(false);
+      });
+  }, []);
+
+  return (
+    <>
+      <div className="content__top">
+        <Categories />
+        <Sort />
+      </div>
+      <h2 className="content__title">Все пиццы</h2>
+      <div className="content__items">
+        {isLoading
+          ? [...new Array(8)].map((_, i) => <Skeleton key={i} />)
+          : items.map((el) => <Pizza key={el.id} {...el} />)}
+      </div>
+    </>
+  );
+};
+
+export default Home;
