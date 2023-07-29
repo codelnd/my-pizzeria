e160731045902e25b0aa8eb964e04185bc37cca3
Denis Bezukladnikov <denis3094@mail.ru>
2023-07-30 00:56:26 +0700

    feat: создать компоненту и роут корзины

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Header from "./components/Header/Header";
 import HomePage from "./pages/HomePage/HomePage";
 import { Route, Routes } from "react-router";
 import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
+import CartPage from "./pages/CartPage/CartPage";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <div className="container">
           <Routes>
             <Route path="/" element={<HomePage />} />
+            <Route path="/cart" element={<CartPage />} />
             <Route path="*" element={<NotFoundPage />} />
           </Routes>
         </div>
