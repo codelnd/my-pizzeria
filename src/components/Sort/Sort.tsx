f0e19673072ffa7d76123f1fb315cdf156d17e65
Denis Bezukladnikov <denis3094@mail.ru>
2023-08-06 23:07:15 +0700

    feat: вывести в коносоль результат фильтрации из запроса

--- src/components/Sort/Sort.tsx
+++ src/components/Sort/Sort.tsx
@@ -8,6 +8,11 @@ const Sort = () => {
   const onSetSelectedCategory = (category) => {
     setSelectedCategory(category);
     setIsOpen(false);
+    fetch(
+      `https://64c0064d0d8e251fd111d86b.mockapi.io/items?sortBy=rating&order=asc`
+    )
+      .then((res) => res.json())
+      .then((data) => console.log(data));
   };
 
   return (
