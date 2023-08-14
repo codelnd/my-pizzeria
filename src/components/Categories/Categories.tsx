92a759239e523f152e60b27f91ce9be8017ff0cf
Denis Bezukladnikov <denis3094@mail.ru>
2023-08-14 23:10:11 +0700

    fix: удалить неиспользуемый код из компоненты категорий

--- src/components/Categories/Categories.tsx
+++ src/components/Categories/Categories.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { SortContext } from "../../App";
 
 const Categories = () => {
   const categories = [
@@ -12,13 +11,12 @@ const Categories = () => {
   ];
 
   const [activeInd, setActiveInd] = React.useState(0);
-  const { setItems } = React.useContext(SortContext);
 
   const onSetActiveInd = (i) => {
     setActiveInd(i);
     fetch(`https://64c0064d0d8e251fd111d86b.mockapi.io/items?category=3`)
       .then((res) => res.json())
-      .then((data) => setItems(data));
+      .then((data) => console.log(data));
   };
 
   return (
