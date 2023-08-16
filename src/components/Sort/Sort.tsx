230e29250f621d205bab5c23b5732e8c1961f9bb
Denis Bezukladnikov <denis3094@mail.ru>
2023-08-16 23:05:39 +0700

    feat: объединить логику фильтрации и сортировки

--- src/components/Sort/Sort.tsx
+++ src/components/Sort/Sort.tsx
@@ -12,12 +12,11 @@ const Sort = () => {
     setSelectedCategory(category);
     setIsOpen(false);
     fetch(
-      `https://64c0064d0d8e251fd111d86b.mockapi.io/items?sortBy=${categoriesOfSort[category]}&order=asc`
+      `https://64c0064d0d8e251fd111d86b.mockapi.io/items?sortBy=${categoriesOfSort[category]}&category=${activeInd}&order=asc`
     )
       .then((res) => res.json())
       .then((data) => setItems(data));
   };
-  // 'https://64c0064d0d8e251fd111d86b.mockapi.io/items?sortBy=rating&category=4&order=asc'
 
   return (
     <section className="sort">
