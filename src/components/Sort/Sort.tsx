d9f44369977efaa9b1383f6a6621fc892f4b8fc6
Denis Bezukladnikov <denis3094@mail.ru>
2023-08-23 23:26:39 +0700

    refactor: причесать код в компоненте сортировки

--- src/components/Sort/Sort.tsx
+++ src/components/Sort/Sort.tsx
@@ -3,19 +3,15 @@ import { SortContext } from "../../App";
 
 const Sort = () => {
   const sortCategories = ["популярности", "цене", "алфавиту"];
-  const [isOpen, setIsOpen] = React.useState(false);
   const categoriesOfSort = ["rating", "price", "title"];
+
+  const [isOpen, setIsOpen] = React.useState(false);
   const [selectedCategory, setSelectedCategory] = React.useState(0);
-  const { setItems } = React.useContext(SortContext);
+  const {} = React.useContext(SortContext);
 
   const onSetSelectedCategory = (category) => {
     setSelectedCategory(category);
     setIsOpen(false);
-    // fetch(
-    //   `https://64c0064d0d8e251fd111d86b.mockapi.io/items?sortBy=${categoriesOfSort[category]}&order=asc`
-    // )
-    //   .then((res) => res.json())
-    //   .then((data) => setItems(data));
   };
 
   return (
